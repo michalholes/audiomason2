443a50131b357e1f496591e6c5a4ce7604725e33
AudioMason <michal@holes.sk>
2026-03-02 18:52:41 +0100

    fff

---
+++ types/am2-globals.d.ts
@@ -0,0 +1,18 @@
+export {};
+
+declare global {
+	interface Window {
+		AM2EditorHTTP: any;
+		AM2WDGraphStable: any;
+		AM2FlowEditorState: any;
+		AM2WDLayoutRoot: any;
+		AM2WDSidebar: any;
+		AM2FlowConfigEditor: any;
+		AM2WDRawError: any;
+		AM2WDTableRender: any;
+		AM2WDDetailsRender: any;
+		AM2WDPaletteRender: any;
+		AM2WDTransitionsRender: any;
+		AM2WizardDefinitionEditor: any;
+	}
+}
